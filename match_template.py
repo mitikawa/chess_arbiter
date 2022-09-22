8b825ec02a88279d26dc30b5ce8de8dd23d56287
mitikawa <92599202+mitikawa@users.noreply.github.com>
2022-09-21 23:52:07 -0300

    Added check king move functionality

--- match_template.py
+++ match_template.py
@@ -1,16 +1,43 @@
 import cv2
 import numpy as np
 
-board_img = cv2.imread('static/board_with_king.png')
-king_img = cv2.imread('static/king_on_white_background.jpg')
+board_status = cv2.imread('static/board_with_king.png')
+board_valid_move = cv2.imread('static/board_valid_move.jpg')
+board_illegal_move = cv2.imread('static/board_illegal_move.jpg')
+king_template = cv2.imread('static/king_on_white_background.jpg')
+square_pixel_size = int(board_status.shape[0]/8)
 
-result = cv2.matchTemplate(board_img, king_img,cv2.TM_CCOEFF_NORMED)
+def find_king(board, template=king_template,method=cv2.TM_CCOEFF_NORMED):
+    match_template_result = cv2.matchTemplate(board, template,method)
+    _, _, _, max_loc = cv2.minMaxLoc(match_template_result)
+    return (max_loc[0]//square_pixel_size,max_loc[1]//square_pixel_size)
 
-_, _, _, max_loc = cv2.minMaxLoc(result)
+def get_square_name(coordinates):
+    columns = 'abcdefgh'
+    square = columns[coordinates[0]]+str(8-coordinates[1])
+    return square
 
-king_int_coordinates = (max_loc[0]//73,max_loc[1]//73)
-columns = 'abcdefgh'
-king_square = columns[king_int_coordinates[0]]+str(8-king_int_coordinates[1])
+def check_king_move(original_coordinate, next_coordinate):
+    # King moved to a non-neighboring square
+    if abs(original_coordinate[0]-next_coordinate[0])>1 or abs(original_coordinate[1]-next_coordinate[1])>1:
+        print(get_square_name(original_coordinate) + "->" + get_square_name(next_coordinate) + " is an illegal king move.")
+        return 1
+    # King did not move
+    elif abs(original_coordinate[0]-next_coordinate[0])==0 and abs(original_coordinate[1]-next_coordinate[1])==0:
+        print("The king did not move.")
+        return 1
 
-print(king_square)
+    # King moved to a neighboring square
+    print(get_square_name(original_coordinate) + "->" + get_square_name(next_coordinate) + " is a valid king move.")
+    return 0 
+
+
+original_king   = find_king(board_status)
+valid_king      = find_king(board_valid_move)
+illegal_king    = find_king(board_illegal_move)
+
+check_king_move(original_king, original_king)
+check_king_move(original_king, valid_king)
+check_king_move(original_king, illegal_king)
+
 
